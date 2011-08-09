94c4d74a8d0a2c3fbd9025f0f681e336c01b18e6
Bram (at EBU) <Bram (at EBU)@localhost>
2011-08-09 19:14:28 +0200

    hotspotd: openmokast remote control

--- hotspotd/devicecontrollers.py
+++ hotspotd/devicecontrollers.py
@@ -8,6 +8,8 @@
 # - enable/disable streaming, and get corresponding url
 # - (and therefore, control the the streaming program)
 
+from openmokast_dbus_remote import *
+
 class DeviceController(object):
     """Abstract class defining what functions a device controller has to implement"""
 
@@ -21,6 +23,12 @@ class DeviceController(object):
         raise NotImplementedError()
 
     def set_frequency(self, frequency):
+        raise NotImplementedError()
+
+    def get_programme_list(self):
+        raise NotImplementedError()
+
+    def set_programme(self, programme):
         raise NotImplementedError()
 
     def get_additional_info(self):
@@ -40,15 +48,24 @@ class DummyController(DeviceController):
    
     def __init__(self):
         self.dev_id = "dummy0"
+        self._freq = 0
+        self._programme = "foo"
 
     def get_frequency_list(self):
         return [0]
 
     def get_frequency(self):
-        return 0
+        return self._freq
 
     def set_frequency(self, frequency):
-        pass
+        self._freq = frequency
+
+    def get_programme_list(self):
+        return ["foo", "bar"]
+
+    def set_programme(self, programme):
+        self._programme = programme
+        return True
 
     def get_additional_info(self):
         return "(No additional info available for dummy device)"
@@ -66,8 +83,67 @@ class DummyController(DeviceController):
 class DABController(DeviceController):
     def __init__(self):
         self.dev_id = "dab0"
+
+        self.rc = OpenmokastReceiverRemote()
+
+        self._programme = None
+        self._destination = {}
+
+        #self.set_frequency(self.get_frequency_list()[0]) # tune to first available freq
+        #try:
+        #    print(self.rc.start_decoding_programme("COULEUR 3"))
+        #except ProgrammeNotInEnsembleError:
+        #    print("COULEUR 3 not found")
+
     def get_frequency_list(self):
         return [223936000]
+
+    def set_frequency(self, frequency):
+        print("Tuning openmokast to {0} Hz".format(frequency))
+        self.rc.tune(frequency)
+
+    def get_frequency(self):
+        return self.rc.get_frequency()
+
+    def get_programme(self):
+        if self._programme is None:
+            return ""
+        return self._programme
+
+    def get_programme_list(self):
+        ens = self.rc.get_ensemble()
+        #print("Ensemble: {0}".format(ens))
+        return ens
+
+    def set_programme(self, programme):
+        print("is {0} in {1}".format(programme, self.rc.get_ensemble()))
+        if programme in self.rc.get_ensemble():
+            self._programme = programme
+            return True
+        else:
+            return False
+
+    def get_additional_info(self):
+        raise NotImplementedError()
+
+    def get_stream_url(self):
+        if self._programme is None:
+            return None
+
+        if self._programme in self._destination:
+            return self._destination[self._programme]
+        else:
+            return ""
+
+    def start_stream(self):
+        if self._programme is None:
+            return False
+        else:
+            self._destination[self._programme] = self.rc.start_decoding_programme(self._programme)
+
+    def stop_stream(self): # who cares ?
+        return True
+
 
 class DVBController(DeviceController):
     def __init__(self):
--- hotspotd/hotspotd.py
+++ hotspotd/hotspotd.py
@@ -4,19 +4,29 @@
 
 Communicates with user device"""
 
-from service import *
+from xml.etree import ElementTree as ET
+from tech import *
 
 from misc import capabilities
 
 import BaseHTTPServer
 import urlparse
-import cgi
 
 # The urls must be http://host/hotspot/<something>
 base_url = "hotspot"
 
 class HotspotHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
+        return self.do_post_and_get('GET')
+        
+    def do_POST(self):
+        return self.do_post_and_get('POST')
+
+    def do_post_and_get(self, method):
+        # convenience
+        get = (method == 'GET')
+        post = (method == 'POST')
+
         parsed_url = urlparse.urlparse(self.path)
 
         p = parsed_url.path.split('/')
@@ -27,12 +37,102 @@ class HotspotHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             return
 
         if p[2] == "capabilities":
-            message = capabilities(servicelist)
-            self.send_response(200)
-            self.send_header("Content-Type", "text/xml")
+            if get:
+                message = capabilities(techlist)
+                self.send_response(200)
+                self.send_header("Content-Type", "text/xml")
+            if post:
+                self.send_error(405) # method not allowed
+                return
+
+        elif p[2] in [str(i) for i in techlist]:
+            if len(p) < 4:
+                self.send_response(400) # Bad Request
+                message = ""
+            else:
+                cmd = p[3]
+                s = name_to_tech(p[2])
+
+                if cmd == "frequency":
+                    if get:
+                        freq = s.devices[0].get_frequency()
+                        self.send_response(200)
+                        self.send_header("Content-Type", "text/plain")
+
+                        message = str(freq)
+
+                    if post:
+                        content_length = int(self.headers.getheader('Content-Length'))
+                        if content_length == 0:
+                            print("Content length is zero !")
+
+                        freq = self.rfile.read(content_length)
+                        print("new frequency {0}".format(freq))
+
+                        try:
+                            s.devices[0].set_frequency(int(freq))
+                        except ValueError:
+                            self.send_error(400)
+                            return
+                        message = ""
+
+                elif cmd == "programmes": # list of programmes
+                    if get:
+                        programmes = s.devices[0].get_programme_list()
+                        root = ET.Element("xml")
+                        root.attrib['encoding'] = 'utf-8'
+                        
+                        for p in programmes:
+                            p_el = ET.SubElement(root, "programme")
+                            p_el.text = p
+                        message = ET.tostring(root, encoding="utf-8")
+                        self.send_response(200)
+                        self.send_header("Content-Type", "text/xml")
+                    if post:
+                        self.send_error(405) # method not allowed
+                        return
+
+                elif cmd == "programme": # current programme
+                    if get:
+                        programme = s.devices[0].get_programme()
+                        root = ET.Element("xml")
+                        root.attrib['encoding'] = 'utf-8'
+                        
+                        p_el = ET.SubElement(root, "programme")
+                        p_el.attrib['name'] = programme
+
+                        url_el = ET.SubElement(p_el, "url")
+                        url_el.text = s.devices[0].get_stream_url()
+
+                        message = ET.tostring(root, encoding="utf-8")
+                        self.send_response(200)
+                        self.send_header("Content-Type", "text/xml")
+                    if post:
+                        content_length = int(self.headers.getheader('Content-Length'))
+                        if content_length == 0:
+                            print("programme: Content length is zero !")
+
+                        programme = self.rfile.read(content_length)
+                        print("new programme {0}".format(programme))
+
+                        if not s.devices[0].set_programme(programme):
+                            self.send_error(400)
+                            return
+
+                        s.devices[0].start_stream()
+                        message = ""
+
+                else:
+                    self.send_response(400)
+                    self.send_header("Content-Type", "text/plain")
+                    message = ""
+
+
+
 
         else:
             self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
             message = '\n'.join([
                 'cmd {0}',
                 'path {1}',
