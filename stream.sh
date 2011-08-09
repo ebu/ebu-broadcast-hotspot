e55edd6029700cd979e526ad0a1959c50770afed
Bram (at EBU) <Bram (at EBU)@localhost>
2011-08-09 12:41:52 +0200

    stream.sh: extra interface http

--- stream.sh
+++ stream.sh
@@ -3,8 +3,8 @@
 #"#rtp{dst=139.10.10.1,port=6024-6025,sdp=rtsp://10.73.141.81:5004/vid.sdp}"
 
 # Streaming of DVB:
-cvlc -v dvb://frequency=578000000 --sout \
-"#transcode{vcodec=mp4v,vb=400,fps=20,scale=0.3,acodec=mp4a,ab=48,channels=2,samplerate=44100}:gather:rtp{dst=139.10.10.1,port=6024-6025,sdp=rtsp://10.73.141.81:5004/vid.sdp}"
+cvlc  --extraintf http -v dvb://frequency=578000000 --sout \
+"#transcode{vcodec=mp4v,vb=1800,fps=20,scale=1.0,acodec=mp4a,ab=96,channels=2,samplerate=44100}:gather:rtp{dst=139.10.10.1,port=6024-6025,sdp=rtsp://10.73.141.81:5004/vid.sdp}"
 
 # use the following URI on the client:
 #rtsp://10.73.141.81:5004/vid.sdp
